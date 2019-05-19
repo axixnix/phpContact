bd82af01e4f47b72ee26d7d1cde5fbf9cf300a9e
axixnix <henryirons@yahoo.com>
2019-05-19 22:15:53 +0100

    started php contact form

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="https://bootswatch.com/cosmo/bootstrap.min.css">
+    <script src="main.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default">
+  <div class="container">
+  <div class="navbar-header">
+  <a href="index.php" class="navbar-brand">My Websiter</a>
+  </div>
+  </div>
+  </nav> 
+  <div class="container">
+  <form action="" method="post">
+  <div class="form-group">
+  <label for="">Name</label>
+  <input type="text" name="name" class="form-control" value="">
+  </div>
+
+  <div class="form-group">
+  <label for="">Email</label>
+  <input type="text" name="email" class="form-control" value="">
+  </div>
+
+  <div class="form-group">
+  <label for="">Message</label>
+  <input type="text" name="message" class="form-control" value="">
+  </div>
+
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+  </form>
+  </div> 
+</body>
+</html>
