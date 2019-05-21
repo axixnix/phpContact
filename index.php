f981cab9fd0fc11c762244b0568bae7f27096e6e
axixnix <henryirons@yahoo.com>
2019-05-21 23:16:31 +0100

    still working on the php form, bootswatch not working

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="https://bootswatch.com/cosmo/bootstrap.min.css">
+    <link rel="stylesheet"  href="https://bootswatch.com/cosmo/bootstrap.min.css">
     <script src="main.js"></script>
 </head>
 <body>
   <nav class="navbar navbar-default">
   <div class="container">
   <div class="navbar-header">
-  <a href="index.php" class="navbar-brand">My Websiter</a>
+  <a href="index.php" class="navbar-brand">My Website</a>
   </div>
   </div>
   </nav> 
   <div class="container">
-  <form action="" method="post">
+  <form action="" method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
   <div class="form-group">
   <label for="">Name</label>
   <input type="text" name="name" class="form-control" value="">
@@ -30,8 +30,9 @@
 
   <div class="form-group">
   <label for="">Message</label>
-  <input type="text" name="message" class="form-control" value="">
+  <textarea name="message" class="form-control"></textarea>
   </div>
+  <br>
 
   <button type="submit" name="submit" class="btn btn-primary">Submit</button>
   </form>
